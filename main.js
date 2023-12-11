b6f1970b23b8ee298b12fa7e9216d927dfbddd49
nadceles <ncsanmart@gmail.com>
2023-12-11 21:25:12 +0100

    Movement improved

--- main.js
+++ main.js
@@ -2,14 +2,33 @@ var character = document.getElementById('character');
 
 var x_position = 285;
 var y_position = 570;
+var right = false;
+var left = false;
 
 window.addEventListener('keydown', function(e) {
     if(e.key === 'a'){
-        x_position -= 1
+        left = true;
     }
     if(e.key === 'd'){
-        x_position += 1
+        right = true;
     }
-
-    character.style.left = x_position + "px";
 })
+
+window.addEventListener('keyup', function(e) {
+    if(e.key === 'a'){
+        left = false;
+    }
+    if(e.key === 'd'){
+        right = false;
+    }
+})
+
+
+var TimerId = setInterval(update_move, 10);
+
+function update_move(){
+    if(left) x_position -= 2;
+    if(right) x_position += 2;
+    character.style.left = x_position + "px";
+
+}
