82daaefbf37665df99d187146df9e92b76002047
nadceles <ncsanmart@gmail.com>
2023-12-12 20:31:19 +0100

    Added default position dash

--- main.js
+++ main.js
@@ -3,29 +3,50 @@ var character = document.getElementById('character');
 var gravity = 5
 var x_position = 285;
 var y_position = 570;
+
 var right = false;
 var left = false;
-var jump = false
+var last_facing = "right";
+
+var up = false;
+var down = false;
+
 var jump_frames = 0;
-var dash = false;
+
 var dash_frames = 0;
 var dash_speed = 10;
 
+var is_jumping = false;
+var is_dashing = false;
+var is_on_floor = true;
 
-console.log('hola')
+var can_jump = true;
+var can_dash = true;
+
+
+
 window.addEventListener('keydown', function(e) {
     console.log(e.key)
     if(e.key === 'a'){
         left = true;
+        last_facing = "left";
     }
     if(e.key === 'd'){
         right = true;
+        last_facing = "right";
     }
-    if(e.key === ' ' || e.key === 'l'){
-        jump = true;
+    if(e.key === 'w'){
+        up = true;
     }
-    if(e.key === 'k'){
-        dash = true;
+    if(e.key === 's'){
+        down = true;
+    }
+
+    if((e.key === ' ' || e.key === 'l') && can_jump){
+        is_jumping = true;
+    }
+    if((e.key === 'k') && can_dash){
+        is_dashing = true;
     }
 })
 
@@ -36,27 +57,33 @@ window.addEventListener('keyup', function(e) {
     if(e.key === 'd'){
         right = false;
     }
+    if(e.key === "w"){
+        up = false;
+    }
+    if(e.key === "s"){
+        down = false;
+    }
 })
 
 var TimerId = setInterval(update_move, 10);
 
 function update_move(){
-    if(jump){
+    if(is_jumping){
         update_jump();
         gravity = 2
     }
 
-    if(dash){
+    if(is_dashing){
         update_dash()
         gravity = 0;
     }
 
-    if(!dash && !jump){
-        gravity = 5
+    if(!is_dashing && !is_jumping){
+        gravity = 6
     }
 
     if(left){
-        if(dash){
+        if(is_dashing){
             x_position -= 2 * dash_speed;
         }
         else{
@@ -64,24 +91,43 @@ function update_move(){
         }
     }
     if(right){
-        if(dash){
+        if(is_dashing){
             x_position += 2 * dash_speed;
         }
         else{
             x_position += 2;
         }
     }
- 
+
+    if(up && is_dashing){
+        y_position -= 2 * dash_speed;
+    }
+    if(down && is_dashing){
+        y_position += 2 * dash_speed;
+    }
+
+    if(!right && !left && !up && !down && is_dashing){
+        if(last_facing === "right"){
+            x_position += 2 * dash_speed;
+        }
+        else if(last_facing === "left"){
+            x_position -= 2 * dash_speed;
+        }
+    }
+
+
     if(y_position < 570) y_position += gravity;
+    if(y_position >= 570) y_position = 570;
     character.style.left = x_position + "px";
     character.style.top =  y_position + "px";
+    check_status();
 }
 
 function update_jump(){
     jump_frames += 1;
     y_position -= 10;
     if(jump_frames >= 10){
-        jump = false;
+        is_jumping = false;
         jump_frames = 0;
     }
 }
@@ -89,7 +135,33 @@ function update_jump(){
 function update_dash(){
     dash_frames += 1;
     if(dash_frames >= 5){
-        dash = false;
+        is_dashing = false;
         dash_frames = 0;
+    }
+}
+
+function check_status(){
+    if(y_position <570){
+        is_on_floor = false
+    }
+    if(y_position >= 570){
+        is_on_floor = true
+    }
+
+    if(is_on_floor){
+        is_jumping = false;
+        can_jump = true;
+
+        if(!is_dashing){
+            can_dash = true;
+        }
+    }
+    
+    if(is_jumping){
+        can_jump = false;
+    }
+
+    if(is_dashing){
+        can_dash = false;
     }
 }
